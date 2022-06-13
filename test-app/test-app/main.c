69a918297801bfe99cce2336daf090bbee2ec4a9
Daniel Lisovoy <daniel.lisovoy@gmail.com>
2022-06-14 00:57:21 +0200

    add dummy project

---
+++ test-app/test-app/main.c
@@ -0,0 +1,13 @@
+//
+//  main.c
+//  test-app
+//
+//  Created by Daniel Lisovoy on 14.06.2022.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    printf("Hello, World!\n");
+    return 0;
+}
