9ddbd50ed2914500644ee5928c8a441ed12d4eab
Daniel Lisovoy <daniel.lisovoy@gmail.com>
2022-06-14 16:46:04 +0200

    jlh

--- src/scripts/install.sh
+++ src/scripts/install.sh
@@ -4,7 +4,7 @@ params=()
 [[ $PARAM_REPO_UPDATE == true ]] && params+=(--repo-update)
 [[ $PARAM_DEPLOYMENT == true ]] && params+=(--deployment)
 [[ $PARAM_CLEAN_INSTALL == true ]] && params+=(--clean-install)
-[[ $PARAM_PROJECT_DIRECTORY != '.' ]] && params+=("--project-directory $PARAM_PROJECT_DIRECTORY")
+[[ $PARAM_PROJECT_DIRECTORY != '.' ]] && params+=("--project-directory=$PARAM_PROJECT_DIRECTORY")
 
 pwd
 echo "${params[@]}"
