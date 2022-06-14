6d59cfaa8a017ba5e9d61337e90ef6ffd7d80aff
Daniel Lisovoy <daniel.lisovoy@gmail.com>
2022-06-14 17:32:53 +0200

    add echo

--- src/scripts/install.sh
+++ src/scripts/install.sh
@@ -7,5 +7,11 @@ params=()
 [[ $PARAM_PROJECT_DIRECTORY != '.' ]] && params+=("--project-directory=$PARAM_PROJECT_DIRECTORY" )
 
 pwd
+
+echo PARAM_REPO_UPDATE && echo $PARAM_REPO_UPDATE
+echo PARAM_DEPLOYMENT && echo $PARAM_DEPLOYMENT
+echo PARAM_CLEAN_INSTALL && echo $PARAM_CLEAN_INSTALL
+echo PARAM_PROJECT_DIRECTORY && echo $PARAM_PROJECT_DIRECTORY
+
 echo "${params[@]}"
 pod install "${params[@]}"
