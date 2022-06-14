5ef6805555b50ac60f25defeae353dc09acc6616
Daniel Lisovoy <daniel.lisovoy@gmail.com>
2022-06-14 17:34:56 +0200

    it is 1 from circleci

--- src/scripts/install.sh
+++ src/scripts/install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 params=()
-[[ $PARAM_REPO_UPDATE == true ]] && params+=("--repo-update" )
-[[ $PARAM_DEPLOYMENT == true ]] && params+=("--deployment" )
-[[ $PARAM_CLEAN_INSTALL == true ]] && params+=("--clean-install" )
+[[ $PARAM_REPO_UPDATE == 1 ]] && params+=("--repo-update" )
+[[ $PARAM_DEPLOYMENT == 1 ]] && params+=("--deployment" )
+[[ $PARAM_CLEAN_INSTALL == 1 ]] && params+=("--clean-install" )
 [[ $PARAM_PROJECT_DIRECTORY != '.' ]] && params+=("--project-directory=$PARAM_PROJECT_DIRECTORY" )
 
 pwd
