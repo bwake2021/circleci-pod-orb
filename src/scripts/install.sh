d59087b75f681c17f7e4b190c88d264b4c99f43a
Daniel Lisovoy <daniel.lisovoy@gmail.com>
2022-06-14 16:40:43 +0200

    change script

--- src/scripts/install.sh
+++ src/scripts/install.sh
@@ -4,8 +4,8 @@ params=()
 [[ $PARAM_REPO_UPDATE == true ]] && params+=(--repo-update)
 [[ $PARAM_DEPLOYMENT == true ]] && params+=(--deployment)
 [[ $PARAM_CLEAN_INSTALL == true ]] && params+=(--clean-install)
-[[ $PARAM_PROJECT_DIRECTORY == true ]] && params+=(--project-directory)
+[[ $PARAM_PROJECT_DIRECTORY != '.' ]] && params+=("--project-directory $PARAM_PROJECT_DIRECTORY")
 
 pwd
-cat "${params[@]}"
-pod install "${params[@]}"
+echo $params
+pod install $params
