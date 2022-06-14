de382c8b9ffb32d07bdc323b2a3f5906688e0003
Daniel Lisovoy <daniel.lisovoy@gmail.com>
2022-06-14 17:15:54 +0200

    update script

--- src/scripts/install.sh
+++ src/scripts/install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 params=()
-[[ $PARAM_REPO_UPDATE == true ]] && params+=(--repo-update)
-[[ $PARAM_DEPLOYMENT == true ]] && params+=(--deployment)
-[[ $PARAM_CLEAN_INSTALL == true ]] && params+=(--clean-install)
-[[ $PARAM_PROJECT_DIRECTORY != '.' ]] && params+=("--project-directory=$PARAM_PROJECT_DIRECTORY")
+[[ $PARAM_REPO_UPDATE == true ]] && params+=(" --repo-update")
+[[ $PARAM_DEPLOYMENT == true ]] && params+=(" --deployment")
+[[ $PARAM_CLEAN_INSTALL == true ]] && params+=(" --clean-install")
+[[ $PARAM_PROJECT_DIRECTORY != '.' ]] && params+=(" --project-directory=$PARAM_PROJECT_DIRECTORY")
 
 pwd
 echo "${params[@]}"
