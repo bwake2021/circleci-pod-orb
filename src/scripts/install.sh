201f94ff7f7ab74d77f847abaebd63a74bce3e3a
Daniel Lisovoy <daniel.lisovoy@gmail.com>
2022-06-14 17:37:45 +0200

    drop redundant commands

--- src/scripts/install.sh
+++ src/scripts/install.sh
@@ -6,12 +6,4 @@ params=()
 [[ $PARAM_CLEAN_INSTALL == 1 ]] && params+=("--clean-install" )
 [[ $PARAM_PROJECT_DIRECTORY != '.' ]] && params+=("--project-directory=$PARAM_PROJECT_DIRECTORY" )
 
-pwd
-
-echo PARAM_REPO_UPDATE && echo $PARAM_REPO_UPDATE
-echo PARAM_DEPLOYMENT && echo $PARAM_DEPLOYMENT
-echo PARAM_CLEAN_INSTALL && echo $PARAM_CLEAN_INSTALL
-echo PARAM_PROJECT_DIRECTORY && echo $PARAM_PROJECT_DIRECTORY
-
-echo "${params[@]}"
 pod install "${params[@]}"
